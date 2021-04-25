ae125015b7f54bb4151920df1ca1c6d4b814fbd2
Floozutter <me@floozutter.site>
2021-04-24 19:27:14 -0700

    quantize colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
 #include <iostream>
 #include <string>
 
@@ -26,13 +26,22 @@ int main(int argc, char * argv[]) {
     cv::Mat frameIn;
     cv::Mat frameOut;
     while (true) {
+        // get frame
         cap.read(frameIn);
         if (frameIn.empty()) {
             std::cerr << "error: empty frame!" << std::endl;
             break;
         }
+        // under-sample (?)
         cv::resize(frameIn, frameOut, cv::Size(), 1.0/scale, 1.0/scale, cv::INTER_AREA);
         cv::resize(frameOut, frameOut, cv::Size(), scale, scale, cv::INTER_NEAREST);
+        // quantize colors
+        for (auto it = frameOut.begin<cv::Vec3b>(); it != frameOut.end<cv::Vec3b>(); ++it) {
+            for (char c = 0; c < 3; ++c) {
+                (*it)[c] = ((*it)[c] / 128) * 255;
+            }
+        }
+        // show
         cv::imshow("deadfeed", frameOut);
         if (cv::waitKey(5) == 27) {
             break;
