3d6fa00ed39e441b46daa37b9ce16f14eff0833d
Floozutter <me@floozutter.site>
2021-04-25 03:42:27 -0700

    start Config

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,40 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <map>
+#include <variant>
 
-const unsigned int DEFAULT_SCALE = 4;
+struct Config {
+    std::variant<int, std::string> feed{0};
+    unsigned int downsample_scale{1};
+    unsigned int range_per_channel{256};
+    static Config from_map(std::map<std::string, std::string> const & map, std::ostream & log) {
+        log << "initializing config from map..." << std::endl;
+        return Config{};
+    }
+    static Config from_text(std::istream & text, std::ostream & log) {
+        log << "reading config map from text stream..." << std::endl;
+        return Config::from_map({}, log);
+    }
+    static Config from_file(std::string const & filename, std::ostream & log) {
+        log << "opening config file as text stream..." << std::endl;
+        return Config::from_text(std::istringstream{}, log);
+    }
+};
 
 int main(int argc, char * argv[]) {
-    if (argc < 2) { 
-        std::cerr << "error: no filename argument!" << std::endl;
+    if (argc < 2) {
+        std::cerr << "error: no config filename argument!" << std::endl;
         return 1;
     }
-    const unsigned int scale = argc < 3 ? DEFAULT_SCALE : std::stoi(argv[2]);
+    Config const cfg{Config::from_file(argv[1], std::cout)};
     cv::VideoCapture cap;
-    if (std::string(argv[1]) == "0") {
-        cap.open(0);
+    if (std::holds_alternative<int>(cfg.feed)) {
+        cap.open(std::get<int>(cfg.feed));
     } else {
-        cap.open(argv[1]);
+        cap.open(std::get<std::string>(cfg.feed));
     }
     if (!cap.isOpened()) {
         std::cerr << "error: can't open capture!" << std::endl;
@@ -33,12 +52,12 @@ int main(int argc, char * argv[]) {
             break;
         }
         // under-sample (?)
-        cv::resize(frameIn, frameOut, cv::Size(), 1.0/scale, 1.0/scale, cv::INTER_AREA);
-        cv::resize(frameOut, frameOut, cv::Size(), scale, scale, cv::INTER_NEAREST);
+        cv::resize(frameIn, frameOut, cv::Size(), 1.0/cfg.downsample_scale, 1.0/cfg.downsample_scale, cv::INTER_AREA);
+        cv::resize(frameOut, frameOut, cv::Size(), cfg.downsample_scale, cfg.downsample_scale, cv::INTER_NEAREST);
         // quantize colors
         for (auto it = frameOut.begin<cv::Vec3b>(); it != frameOut.end<cv::Vec3b>(); ++it) {
             for (char c = 0; c < 3; ++c) {
-                (*it)[c] = ((*it)[c] / 128) * 255;
+                (*it)[c] = ((*it)[c] / (256 / cfg.range_per_channel)) * (255 / (cfg.range_per_channel - 1));
             }
         }
         // show
