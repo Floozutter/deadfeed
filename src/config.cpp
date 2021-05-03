4bb0815ba303214f14c659f568e2a8e161916c65
Floozutter <me@floozutter.site>
2021-05-02 23:56:53 -0700

    use fmt functions in config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -1,11 +1,12 @@
 #include "config.hpp"
+#include "fmt.hpp"
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
 
 Config Config::from_map(std::map<std::string, std::string> const & map, std::ostream & log) {
-    log << "initializing config from map..." << std::endl;
+    log << fmt::header("initializing config from map") << std::endl;
     Config cfg{};
     {
         auto const & it = map.find("downsample_scale");
@@ -30,16 +31,17 @@ Config Config::from_map(std::map<std::string, std::string> const & map, std::ost
 }
 
 Config Config::from_text(std::istream & text, std::ostream & log) {
-    log << "reading config map from text stream..." << std::endl;
+    log << fmt::header("reading config map from text stream") << std::endl;
     std::map<std::string, std::string> map{};
     for (std::string line; std::getline(text, line);) {
         auto const & partition = line.find("=");
         if (partition == std::string::npos) {
-            log << "- warning: no delimiter found!" << std::endl;
+            log << fmt::item(fmt::warning("no delimiter found")) << std::endl;
         } else {
             std::string const & key{line.substr(0, partition)};
             std::string const & value{line.substr(partition + 1)};
-            log << "- read key-value pair `" << key << "`, `" << value << "`." << std::endl;
+            log << fmt::item(fmt::result("read key-value pair `" + key + "`, `" + value + "`"))
+                << std::endl;
             map.insert({key, value});
         }
     }
@@ -47,12 +49,12 @@ Config Config::from_text(std::istream & text, std::ostream & log) {
 }
 
 Config Config::from_file(std::string const & filename, std::ostream & log) {
-    log << "opening config file `" << filename << "` as text stream..." << std::endl;
+    log << fmt::header("opening config file `" + filename + "` as text stream") << std::endl;
     std::ifstream ifile{filename};
     if (ifile) {
         return Config::from_text(ifile, log);
     } else {
-        log << "error: can't open file!" << std::endl;
+        log << fmt::error("can't open file") << std::endl;
         return Config{};
     }
 }
