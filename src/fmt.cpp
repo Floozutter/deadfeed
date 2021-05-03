9548ffb4e0a1f74fd43c9db8282f899cccbf8a6b
Floozutter <me@floozutter.site>
2021-05-03 01:20:49 -0700

    add color?

--- src/fmt.cpp
+++ src/fmt.cpp
@@ -1,6 +1,11 @@
 #include "fmt.hpp"
 #include <string>
 
+std::string const DEFAULT{"\033[39;49m"};
+std::string const RED{"\033[31m"};
+std::string const GREEN{"\033[32m"};
+std::string const YELLOW{"\033[33m"};
+
 std::string fmt::header(std::string const & text) {   
     return text + "...";
 }
@@ -18,9 +23,9 @@ std::string fmt::result(std::string const & text) {
 }
 
 std::string fmt::warning(std::string const & text) {
-    return "warning: " + text + "!";
+    return YELLOW + "warning: " + text + "!" + DEFAULT;
 }
 
 std::string fmt::error(std::string const & text) {
-    return "error: " + text + "!";
+    return RED + "error: " + text + "!" + DEFAULT;
 }
