56d879e4286820b654df76224333aae6810a1f73
Floozutter <me@floozutter.site>
2021-05-02 23:03:33 -0700

    use namespace fmt

--- src/fmt.cpp
+++ src/fmt.cpp
@@ -1,26 +1,26 @@
-#include "log.hpp"
+#include "fmt.hpp"
 #include <string>
 
-std::string header(std::string const & text) {   
+std::string fmt::header(std::string const & text) {   
     return text + "...";
 }
 
-std::string item(unsigned int depth, std::string const & text) {
+std::string fmt::item(unsigned int depth, std::string const & text) {
     return std::string(2 * depth - 1, '-') + " " + text;
 }
 
-std::string item(std::string const & text) {
+std::string fmt::item(std::string const & text) {
     return item(1, text);
 }
 
-std::string result(std::string const & text) {
+std::string fmt::result(std::string const & text) {
     return text + ".";
 }
 
-std::string warning(std::string const & text) {
+std::string fmt::warning(std::string const & text) {
     return "warning: " + text + "!";
 }
 
-std::string error(std::string const & text) {
+std::string fmt::error(std::string const & text) {
     return "error: " + text + "!";
 }
--- src/fmt.hpp
+++ src/fmt.hpp
@@ -3,11 +3,13 @@
 
 #include <string>
 
-std::string header(std::string const & text);
-std::string item(unsigned int depth, std::string const & text);
-std::string item(std::string const & text);
-std::string result(std::string const & text);
-std::string warning(std::string const & text);
-std::string error(std::string const & text);
+namespace fmt {
+    std::string header(std::string const & text);
+    std::string item(unsigned int depth, std::string const & text);
+    std::string item(std::string const & text);
+    std::string result(std::string const & text);
+    std::string warning(std::string const & text);
+    std::string error(std::string const & text);
+}
 
 #endif
